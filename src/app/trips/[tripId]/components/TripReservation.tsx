77265c641b2ddf7c2493a2f1915bb0bad62c92ca
Natan Alonso <77931510+Natan-Alonso@users.noreply.github.com>
2023-10-17 09:51:58 -0300

    feat: add date validation to trip reservation

--- src/app/trips/[tripId]/components/TripReservation.tsx
+++ src/app/trips/[tripId]/components/TripReservation.tsx
@@ -7,7 +7,9 @@ import { Trip } from '@prisma/client'
 import { Controller, useForm } from 'react-hook-form'
 
 interface TripReservationProps {
-  trip: Trip
+  tripStartDate: Date
+  tripEndDate: Date
+  maxGuests: number
 }
 
 interface TripReservationForm {
@@ -16,17 +18,24 @@ interface TripReservationForm {
   endDate?: Date | null
 }
 
-const TripReservation = ({ trip }: TripReservationProps) => {
+const TripReservation = ({
+  maxGuests,
+  tripStartDate,
+  tripEndDate,
+}: TripReservationProps) => {
   const {
     register,
     handleSubmit,
     formState: { errors },
     control,
+    watch,
   } = useForm<TripReservationForm>()
 
   const onSubmit = (data: any) => {
     console.log({ data })
   }
+
+  const startDate = watch('startDate')
 
   return (
     <div className="flex flex-col px-5">
@@ -48,6 +57,7 @@ const TripReservation = ({ trip }: TripReservationProps) => {
               selected={field.value}
               placeholderText="Data de Início"
               className="w-full"
+              minDate={tripStartDate}
             />
           )}
         />
@@ -68,6 +78,8 @@ const TripReservation = ({ trip }: TripReservationProps) => {
               selected={field.value}
               placeholderText="Data Final"
               className="w-full"
+              maxDate={tripEndDate}
+              minDate={startDate ?? tripStartDate}
             />
           )}
         />
@@ -80,7 +92,7 @@ const TripReservation = ({ trip }: TripReservationProps) => {
             message: 'Número de hóspedes é obrigatório!',
           },
         })}
-        placeholder={`Número de hóspedes (max: ${trip.maxGuests})`}
+        placeholder={`Número de hóspedes (max: ${maxGuests})`}
         className="mt-4"
         error={!!errors?.guests}
         errorMessage={errors?.guests?.message}
