4019d5e5ff9fd02fc7e4cf44590ebaeb3e7a5d63
Natan Alonso <77931510+Natan-Alonso@users.noreply.github.com>
2023-10-20 10:05:37 -0300

    feat: add trip buy

--- src/app/trips/[tripId]/confirmation/page.tsx
+++ src/app/trips/[tripId]/confirmation/page.tsx
@@ -11,6 +11,7 @@ import { useSession } from 'next-auth/react'
 import Button from '@/components/Button'
 
 import { Trip } from '@prisma/client'
+import { toast } from 'react-toastify'
 
 const TripConfirmation = ({ params }: { params: { tripId: string } }) => {
   const [trip, setTrip] = useState<Trip | null>()
@@ -18,7 +19,7 @@ const TripConfirmation = ({ params }: { params: { tripId: string } }) => {
 
   const router = useRouter()
 
-  const { status } = useSession()
+  const { status, data } = useSession()
 
   const searchParams = useSearchParams()
 
@@ -48,9 +49,37 @@ const TripConfirmation = ({ params }: { params: { tripId: string } }) => {
     }
 
     fetchTrip()
-  }, [status])
+  }, [params, router, searchParams, status])
 
   if (!trip) return null
+
+  const handleBuyClick = async () => {
+    const res = await fetch('http://localhost:3000/api/trips/reservation', {
+      method: 'POST',
+      body: Buffer.from(
+        JSON.stringify({
+          tripId: params.tripId,
+          startDate: searchParams.get('startDate'),
+          endDate: searchParams.get('endDate'),
+          guests: Number(searchParams.get('guests')),
+          userId: (data?.user as any)?.id,
+          totalPaid: totalPrice,
+        }),
+      ),
+    })
+
+    if (!res.ok) {
+      return toast.error('Ocorreu um erro ao realizar a reserva!', {
+        position: 'bottom-center',
+      })
+    }
+
+    router.push('/')
+
+    toast.success('Reserva realizada com sucesso!', {
+      position: 'bottom-center',
+    })
+  }
 
   const startDate = new Date(searchParams.get('startDate') as string)
   const endDate = new Date(searchParams.get('endDate') as string)
@@ -107,7 +136,9 @@ const TripConfirmation = ({ params }: { params: { tripId: string } }) => {
         <h3 className="font-semibold mt-5">Hóspedes</h3>
         <p>{guests} hóspedes</p>
 
-        <Button className="mt-5">Finalizar Compra</Button>
+        <Button onClick={handleBuyClick} className="mt-5">
+          Finalizar Compra
+        </Button>
       </div>
     </div>
   )
